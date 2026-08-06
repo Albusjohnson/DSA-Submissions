8446bc0574fb75135e5eea7cb1db617185aecbe1
Albusjohnson <93551461+Albusjohnson@users.noreply.github.com>
2026-08-07 04:34:13 +0530

    Add: string-encode-and-decode - submission-5

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-5.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+
+    string encode(vector<string>& strs) {
+        string test = "";
+        for(int i=0; i<strs.size();i++){
+            test = test + to_string(strs[i].size()) + '#' + strs[i];
+        }
+        // cout<<test<<endl;
+        return test;
+    }
+
+    vector<string> decode(string s) {
+        vector<string> ans;
+        int i=0;
+        while(i<s.size()){
+            string temp = "";
+            string num = "";
+            while(i<s.size()&&s[i]!='#'){
+                num=num+s[i];
+                i++;
+            }
+            i++;
+            // cout<<num<<endl;
+            int numInt = stoi(num);
+            for(int j=0;j<numInt&&i<s.size();j++){
+                temp=temp+s[i];
+                i++;
+            }
+            ans.push_back(temp);
+            temp="";
+        }
+        // ans.push_back(temp);
+        return ans;
+    }
+};
