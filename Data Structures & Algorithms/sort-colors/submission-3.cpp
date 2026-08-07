513fc61a6eb3fcc806c32d7bb749167dc03431e4
Albusjohnson <93551461+Albusjohnson@users.noreply.github.com>
2026-08-07 06:30:08 +0530

    Add: sort-colors - submission-3

---
+++ Data Structures & Algorithms/sort-colors/submission-3.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    void printa(vector<int>&nums){
+        for(auto i:nums){
+            cout<<i<<" ";
+        }
+        cout<<endl;
+    }
+    void sortColors(vector<int>& nums) {
+        int z =0;
+        int o =0;
+        for(int i=0;i<nums.size();i++){
+            if(!nums[i]){
+                swap(nums[i],nums[z]);
+                z++;
+                o=max(o,z);
+            }
+            if(nums[i]==1){
+                swap(nums[i],nums[o]);
+                o++;
+            }
+            printa(nums);
+        }
+    }
+};
